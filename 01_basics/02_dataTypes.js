d8609d32b25a0500b90e71b7a0e637666e1d9393
Yogesh-2403 <123499211+Yogesh-2403@users.noreply.github.com>
2024-08-06 04:35:41 +0000

    container commit done

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,21 @@
+"use strict"; //treat all js code as newer version
+
+//alert(3 + 3) we using node not browser
+console.log(3+ 
+    3
+);//code readability must be there
+
+let name ="Yogesh"
+let age = 21
+let isLoggedIn = false
+/*number 2^53
+bigint
+string ""
+boolean true/false
+null = standlone value
+undefined value not assigned
+symbol = unique*/
+
+
+console.log(typeof undefined) //undefined
+console.log(typeof null)  //object
